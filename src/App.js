7d490edbbb94b48b38d98ba5f8c63893af57b2ab
danielkim85 <danielkim@soulkast.com>
2021-12-25 18:14:10 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,159 @@
-import logo from './logo.svg';
+import React, { useEffect, useState, useCallback } from 'react';
+import io from 'socket.io-client';
 import './App.css';
 
 function App() {
+  const [socket, setSocket] = useState(null);
+  const [codes, setCodes] = useState('');
+  const [msg, setMsg] = useState('');
+  const [output, setOutput] = useState('');
+  const [joined, setJoined] = useState(false);
+  const [init, setInit] = useState(false);
+  const [statusMsg, setStatusMsg] = useState('');
+  const [copyButtonText, setCopyButtonText] = useState('');
+  let codeRefs = {};
+
+  useEffect(() => {
+    if(!socket) {
+      console.log('setting socket');
+      const newSocket = io(`http://${window.location.hostname}:3011`);
+      setSocket(newSocket);
+    }
+  }, []);
+
+  useEffect(() => {
+    if(!socket) { //this will always run first time, return if socket not ray
+      return;
+    }
+
+    console.log('registering socket events');
+    //socket events
+    socket.on('error', handleError);
+    socket.on('msg', handleMsg);
+    socket.on('created', handleCreated);
+    socket.on('joined', handleJoined);
+
+    return () => {
+      // before the component is destroyed
+      // unbind all event handlers used in this component
+      socket.off("created", handleCreated);
+    };
+  }, [socket]);
+
+  const handleCreated = (msg) => {
+    for(let i = 0; i < msg.length; i++) {
+      codeRefs[i].value = msg[i];
+    }
+    setCodes(msg);
+    setStatusMsg('waiting ...');
+    setInit(true);
+  };
+
+  const handleJoined = (msg) => {
+    if(msg) { // contains visitor's id
+      setStatusMsg('');
+      setJoined(true);
+    }
+    else { // this is the visitor
+      setStatusMsg('waiting ...');
+      setInit(true);
+    }
+  };
+
+  const handleMsg = (msg) => {
+    setStatusMsg('');
+    setOutput(msg);
+  };
+
+  const handleError = (msg) => {
+    alert(msg);
+  };
+
+  const doAction = (action,value) => {
+    socket.emit(action,value);
+  };
+
+  const inputCode = (pos,value) => {
+    if(value === '') return;
+
+    let codes = '';
+    for(const code in codeRefs) {
+      codes += codeRefs[code].value.toUpperCase();
+    }
+
+    if(pos < 5) {
+      codeRefs[pos+1].focus();
+    }
+
+    if(codes.length >= 6) {
+      doAction('join',codes);
+    }
+  };
+
+  const codeOnFocus = (pos) => {
+    if(pos === 0) return;
+
+    if(codeRefs[pos-1].value !== '') return;
+    codeRefs[codes.length].focus();
+  };
+
+  const copy = () => {
+    navigator.clipboard.writeText(output).then(function() {
+      setCopyButtonText('copied');
+    }, function(err) {
+      console.error('Async: Could not copy text: ', err);
+    });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="code">
+        {
+          Array(6).fill().map((el,i) => {
+            return <input key={i}
+                          ref={ e => codeRefs[i] = e}
+                          autoFocus={ i === 0 }
+                          maxLength="1"
+                          disabled={ init }
+                          onFocus={ () => codeOnFocus(i) }
+                          onInput={ e => inputCode(i, e.target.value) }/>
+          })
+        }
+      </div>
+      { !init &&
+        <div className="action">
+          <button onClick={ () => doAction('create') }>
+            create
+          </button>
+        </div>
+      }
+      { joined &&
+        <div className="message">
+          <input value={msg}
+                 onInput={e => setMsg(e.target.value)}/>
+          <button onClick={() => {
+            doAction('send', {code: codes, msg: msg});
+            setMsg('');
+          }}>
+            send
+          </button>
+        </div>
+      }
+      { output !== '' &&
+        <div className="output">
+          <div className="inner">
+            {output}
+          </div>
+          <button onClick={copy}>
+            {copyButtonText === '' ?  'copy' : copyButtonText}
+          </button>
+        </div>
+      }
+      { statusMsg !== '' &&
+        <div className="status">
+          {statusMsg}
+        </div>
+      }
     </div>
   );
 }
---
+++ src/socket.js
@@ -0,0 +1,91 @@
+const express = require('express'); // using express
+const socketIO = require('socket.io');
+const http = require('http')
+const cors = require('cors');
+
+const port = process.env.PORT || 3011 // setting the port
+let app = express();
+app.use(cors());
+
+let server = http.createServer(app)
+let io = socketIO(server, {
+  cors: {
+    origin: ['http://localhost:3010'],
+    methods: ['GET', 'POST']
+  }
+});
+
+server.listen(port);
+console.log('server running on ' + port);
+
+const generateCode = () => {
+  return Math.random().toString(36).substring(2,8).toUpperCase().replaceAll('0','Z').replaceAll('O','X');
+};
+
+const MAX_SESSIONS_SIZE = 1000;
+let sessions = {};
+let idToCode = {};
+
+io.on('connection', (socket)=>{
+
+  socket.on('create', () => {
+    if(Object.keys(sessions).length > MAX_SESSIONS_SIZE) {
+      io.to(socket.id).emit('error','Maximum sessions reached');
+      return;
+    }
+
+    let code;
+    do {
+      code = generateCode();
+      if(!sessions[code]) {
+        sessions[code] = {
+          creator:socket.id
+        }
+      }
+    } while (!sessions[code] );
+    idToCode[socket.id] = code;
+    io.to(socket.id).emit('created',code);
+  });
+
+  socket.on('join', (code) => {
+    if(!sessions[code]) {
+      io.to(socket.id).emit('error','invalid code');
+      return;
+    }
+    sessions[code].guest = socket.id
+    io.to(sessions[code].creator).emit('joined',sessions[code].guest);
+    io.to(socket.id).emit('joined');
+  });
+
+  socket.on('send', (data) => {
+    const code = data.code;
+    if(!sessions[code]) {
+      io.to(socket.id).emit('error','invalid code');
+      return;
+    }
+    let to;
+    if(sessions[code].creator === socket.id) {
+      to = sessions[code].guest;
+    }
+    else if(sessions[code].guest === socket.id) {
+      to = sessions[code].creator;
+    }
+    else {
+      io.to(socket.id).emit('error','you are not welcome!');
+      return;
+    }
+    io.to(to).emit('msg', data.msg);
+  });
+
+  //clean up
+  socket.on('disconnect', () => {
+    const code = idToCode[socket.id];
+    if(!code) return;
+    delete idToCode[socket.id];
+    delete sessions[code];
+  });
+
+  socket.on('ping', (msg) => {
+    io.to(socket.id).emit('msg','pong');
+  });
+});
