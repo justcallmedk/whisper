6d82a404f806e953c7781a444664b13926cc1d4a
danielkim85 <danielkim@soulkast.com>
2021-12-25 22:50:55 -0500

    removed hardcoded port number

--- src/App.js
+++ src/App.js
@@ -16,8 +16,8 @@ function App() {
   useEffect(() => {
     if(!socket) {
       console.log('setting socket (should not run more than once)');
-      const port = window.location.hostname === 'localhost' ? '3011' : '80';
-      const newSocket = io(`//${window.location.hostname}:` + port, {path:'/socket/io'});
+      const port = window.location.hostname === 'localhost' ? ':3011' : '';
+      const newSocket = io(`//${window.location.hostname}` + port, {path:'/socket/io'});
       setSocket(newSocket);
     }
   }, []);
